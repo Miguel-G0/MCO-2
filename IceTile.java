985219f40186548821bb234e88d816780896fd6b
So-dl <clarence_so@dlsu.edu.ph>
2025-11-25 17:46:37 +0800

    Update IceTile.java

--- IceTile.java
+++ IceTile.java
@@ -1,25 +1,16 @@
 public class IceTile extends Tiles {
-    private String lastDirection;
 
     public IceTile(String tileName) {
-        super(tileName, true, 'I');
-        this.sign = 'I';
-        this.lastDirection = null;
+        super(tileName, true, 'i');
+        this.sign = 'i';
     }
 
-    @Override
+
     public boolean isIceTile() {
         return true;
     }
 
-    @Override
-    public String getLastDirection() {
-        return lastDirection;
-    }
 
 
-    public void handlePlayerEntry(String moveDirection) {
-        this.lastDirection = moveDirection;
-    }
 
 }
